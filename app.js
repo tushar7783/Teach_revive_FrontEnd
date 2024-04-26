c64ccc059bb30c73c7cffc2376e6da6070f7005b
shri67gh <shristi.110105@gmail.com>
2024-04-26 23:03:04 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+//console.log("Hello World!");
+//console.log("Apna file");
+let a = 9;
+let b = 8;
+console.log("sum is: ", a + b);
+// this is a comments
+
---
+++ app1.js
@@ -0,0 +1,7 @@
+let pencilprice = 10;
+let eraserprice = 5;
+// let output = "the total price is : " + (pencilprice + eraserprice) + " Rupees. ";
+// console.log("the total price is : ", pencilprice + eraserprice," Rupees. ");
+let output = `The total price is :${pencilprice + eraserprice} Rupees. `;
+console.log(output);
+
