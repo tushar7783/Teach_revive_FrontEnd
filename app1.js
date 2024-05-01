53a91836fabb0adaf444971deef432ae0fb2c9da
shri67gh <shristi.110105@gmail.com>
2024-05-01 22:08:18 +0530

    new register

--- app1.js
+++
@@ -1,7 +0,0 @@
-let pencilprice = 10;
-let eraserprice = 5;
-// let output = "the total price is : " + (pencilprice + eraserprice) + " Rupees. ";
-// console.log("the total price is : ", pencilprice + eraserprice," Rupees. ");
-let output = `The total price is :${pencilprice + eraserprice} Rupees. `;
-console.log(output);
-
